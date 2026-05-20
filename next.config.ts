813b989927b955ff0bf5cc767936428c82abac06
Himanshu Sharma <himanshu9595.sharma@gmail.com>
2026-05-21 00:55:05 +0530

    bug fixes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async rewrites() {
+//     if (process.env.NODE_ENV === 'development') {
+//       return [
+//         {
+//           source: '/api/:path*',
+//           destination: 'https://delhichaska-backend.onrender.com/api/:path*',
+//         },
+//       ]
+//     }
+//     return []
+//   },
+// }
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://delhichaska-backend.onrender.com/api/:path*',
-        },
-      ]
-    }
-    return []
+
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          'https://delhichaska-backend.onrender.com/api/:path*',
+      },
+    ]
   },
 }
 

36308db79c6b6d7128b05e5394ee38fd1eb55c44
Himanshu Sharma <himanshu9595.sharma@gmail.com>
2026-05-22 22:02:22 +0530

    UX changes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,91 @@
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+// export function proxy(
+//     request: NextRequest
+// ) {
+
+//     const { pathname } =
+//         request.nextUrl
+
+//     // READ COOKIE
+//     const token =
+//         request.cookies
+//             .get('accesstoken')
+//             ?.value
+
+//     // PROTECTED ROUTES
+//     const protectedRoutes = [
+//         '/menu',
+//         '/checkout',
+//         '/profile/cart',
+//         '/profile/orders',
+//         '/profile/addresses',
+//         '/profile/personal-info',
+//     ]
+
+//     const isProtected =
+//         protectedRoutes.some(route =>
+//             pathname.startsWith(route)
+//         )
+
+//     // NOT LOGGED IN
+//     if (
+//         !token &&
+//         isProtected
+//     ) {
+
+//         return NextResponse.redirect(
+//             new URL(
+//                 '/profile',
+//                 request.url
+//             )
+//         )
+//     }
+
+//     // LOGGED IN USER
+//     // SHOULD NOT ACCESS AUTH PAGES
+//     if (
+//         token &&
+//         (
+//             pathname === '/login' ||
+//             pathname === '/signup' ||
+//             pathname === '/verify-otp'
+//         )
+//     ) {
+
+//         return NextResponse.redirect(
+//             new URL(
+//                 '/',
+//                 request.url
+//             )
+//         )
+//     }
+
+//     return NextResponse.next()
+// }
+
+// export const config = {
+//     matcher: [
+//         '/menu/:path*',
+//         '/checkout/:path*',
+//         '/profile/cart/:path*',
+//         '/profile/orders/:path*',
+//         '/profile/addresses/:path*',
+//         '/profile/personal-info/:path*',
+//         '/login',
+//         '/signup',
+//         '/verify-otp',
+//     ],
+// }
+
+
+
+
+
+
+
+
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
@@ -8,7 +96,7 @@ export function proxy(
     const { pathname } =
         request.nextUrl
 
-    // READ COOKIE
+    // ACCESS TOKEN
     const token =
         request.cookies
             .get('accesstoken')
@@ -29,7 +117,7 @@ export function proxy(
             pathname.startsWith(route)
         )
 
-    // NOT LOGGED IN
+    // BLOCK IF NOT LOGGED IN
     if (
         !token &&
         isProtected
@@ -37,14 +125,13 @@ export function proxy(
 
         return NextResponse.redirect(
             new URL(
-                '/profile',
+                '/login',
                 request.url
             )
         )
     }
 
-    // LOGGED IN USER
-    // SHOULD NOT ACCESS AUTH PAGES
+    // BLOCK AUTH PAGES FOR LOGGED USER
     if (
         token &&
         (

db373c17d3953da64d8fe3648c9529713214f877
Himanshu Sharma <himanshu9595.sharma@gmail.com>
2026-05-22 22:06:31 +0530

    Updates

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,91 +1,3 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
-
-// export function proxy(
-//     request: NextRequest
-// ) {
-
-//     const { pathname } =
-//         request.nextUrl
-
-//     // READ COOKIE
-//     const token =
-//         request.cookies
-//             .get('accesstoken')
-//             ?.value
-
-//     // PROTECTED ROUTES
-//     const protectedRoutes = [
-//         '/menu',
-//         '/checkout',
-//         '/profile/cart',
-//         '/profile/orders',
-//         '/profile/addresses',
-//         '/profile/personal-info',
-//     ]
-
-//     const isProtected =
-//         protectedRoutes.some(route =>
-//             pathname.startsWith(route)
-//         )
-
-//     // NOT LOGGED IN
-//     if (
-//         !token &&
-//         isProtected
-//     ) {
-
-//         return NextResponse.redirect(
-//             new URL(
-//                 '/profile',
-//                 request.url
-//             )
-//         )
-//     }
-
-//     // LOGGED IN USER
-//     // SHOULD NOT ACCESS AUTH PAGES
-//     if (
-//         token &&
-//         (
-//             pathname === '/login' ||
-//             pathname === '/signup' ||
-//             pathname === '/verify-otp'
-//         )
-//     ) {
-
-//         return NextResponse.redirect(
-//             new URL(
-//                 '/',
-//                 request.url
-//             )
-//         )
-//     }
-
-//     return NextResponse.next()
-// }
-
-// export const config = {
-//     matcher: [
-//         '/menu/:path*',
-//         '/checkout/:path*',
-//         '/profile/cart/:path*',
-//         '/profile/orders/:path*',
-//         '/profile/addresses/:path*',
-//         '/profile/personal-info/:path*',
-//         '/login',
-//         '/signup',
-//         '/verify-otp',
-//     ],
-// }
-
-
-
-
-
-
-
-
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
@@ -96,7 +8,7 @@ export function proxy(
     const { pathname } =
         request.nextUrl
 
-    // ACCESS TOKEN
+    // READ COOKIE
     const token =
         request.cookies
             .get('accesstoken')
@@ -117,7 +29,7 @@ export function proxy(
             pathname.startsWith(route)
         )
 
-    // BLOCK IF NOT LOGGED IN
+    // NOT LOGGED IN
     if (
         !token &&
         isProtected
@@ -125,13 +37,14 @@ export function proxy(
 
         return NextResponse.redirect(
             new URL(
-                '/login',
+                '/profile',
                 request.url
             )
         )
     }
 
-    // BLOCK AUTH PAGES FOR LOGGED USER
+    // LOGGED IN USER
+    // SHOULD NOT ACCESS AUTH PAGES
     if (
         token &&
         (
@@ -165,3 +78,90 @@ export const config = {
         '/verify-otp',
     ],
 }
+
+
+
+
+
+
+
+
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+// export function proxy(
+//     request: NextRequest
+// ) {
+
+//     const { pathname } =
+//         request.nextUrl
+
+//     // ACCESS TOKEN
+//     const token =
+//         request.cookies
+//             .get('accesstoken')
+//             ?.value
+
+//     // PROTECTED ROUTES
+//     const protectedRoutes = [
+//         '/menu',
+//         '/checkout',
+//         '/profile/cart',
+//         '/profile/orders',
+//         '/profile/addresses',
+//         '/profile/personal-info',
+//     ]
+
+//     const isProtected =
+//         protectedRoutes.some(route =>
+//             pathname.startsWith(route)
+//         )
+
+//     // BLOCK IF NOT LOGGED IN
+//     if (
+//         !token &&
+//         isProtected
+//     ) {
+
+//         return NextResponse.redirect(
+//             new URL(
+//                 '/login',
+//                 request.url
+//             )
+//         )
+//     }
+
+//     // BLOCK AUTH PAGES FOR LOGGED USER
+//     if (
+//         token &&
+//         (
+//             pathname === '/login' ||
+//             pathname === '/signup' ||
+//             pathname === '/verify-otp'
+//         )
+//     ) {
+
+//         return NextResponse.redirect(
+//             new URL(
+//                 '/',
+//                 request.url
+//             )
+//         )
+//     }
+
+//     return NextResponse.next()
+// }
+
+// export const config = {
+//     matcher: [
+//         '/menu/:path*',
+//         '/checkout/:path*',
+//         '/profile/cart/:path*',
+//         '/profile/orders/:path*',
+//         '/profile/addresses/:path*',
+//         '/profile/personal-info/:path*',
+//         '/login',
+//         '/signup',
+//         '/verify-otp',
+//     ],
+// }
